b7305c74a108ac554c180c88b320f4677c39b532
Kalinowski <35411861+Hellmick@users.noreply.github.com>
2023-02-26 14:10:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var curIndex = 1;
+const maxImage = 2;
+const img = './static/img/slider/image*_blue.jpeg'
+const duration = 10000;
+
+function slider() {
+    setInterval(function() {
+        let source = "url('./static/img/slider/image" + image.toString() + "_blue.jpeg')";
+        document.getElementById("sliderImage").style.backgroundImage = source;
+        image++;
+        if (image > maxImage) image = 1;
+        console_log(image);
+    }, "10000");
+}
+
+function sliderNext() {
+    image++;
+    if (image > maxImage) image = 1;
+    let source = "url('./static/img/slider/image" + image.toString() + "_blue.jpeg')";
+    document.getElementById("sliderImage").style.backgroundImage = source;
+}
+
+function sliderPrevious() {
+    image--;
+    if (image < 1) image = maxImage;
+    let source = "url('./static/img/slider/image" + image.toString() + "_blue.jpeg')";
+    document.getElementById("sliderImage").style.backgroundImage = source;
+}
